46eaba3cc2f56ba2746a8fef5f122e8c2d7847c4
Max Chung <cwonho16@gmail.com>
2023-01-22 02:29:13 +0900

    feat: useReducer 이용해 일기 데이터 상태변화 로직 작성 & 데이터 state와 state의 dispatch 함수들을 공급할  Context 생성 후 Provider로 공급

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useReducer, useRef } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
@@ -5,65 +6,85 @@ import Detail from "./pages/Detail";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
 
-import Button from "./components/Button";
-import Header from "./components/Header";
+const reducer = (state, action) => {
+	let newState = [];
+	switch (action.type) {
+		case "INIT": {
+			return action.data;
+		}
+		case "CREATE": {
+			newState = [action.data, ...state];
+			break;
+		}
+		case "EDIT": {
+			newState = state.map((item) =>
+				item.id === action.data.targetId ? { ...action.data } : item
+			);
+			break;
+		}
+		case "DELETE": {
+			newState = state.filter((item) => item.id !== action.targetId);
+			break;
+		}
+		default:
+			return state;
+	}
+	return newState;
+};
+
+export const DiaryStateContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
 
 function App() {
+	const [list, dispatch] = useReducer(reducer, []);
+
+	const listId = useRef(0);
+
+	const onCreate = (date, content, emotion) => {
+		dispatch({
+			type: "CREATE",
+			data: {
+				id: listId.currentdate,
+				date: new Date(date).getTime(),
+				content,
+				emotion,
+			},
+		});
+
+		listId.current += 1;
+	};
+
+	const onEdit = (targetId, date, content, emotion) => {
+		dispatch({
+			type: "EDIT",
+			data: {
+				id: targetId,
+				date: new Date(date).getTime(),
+				content,
+				emotion,
+			},
+		});
+	};
+
+	const onDelete = (targetId) => {
+		dispatch({ type: "DELETE", targetId });
+	};
+
 	return (
-		<BrowserRouter>
-			<div className="App">
-				<Header
-					headText={"App"}
-					leftChild={
-						<Button
-							text={"왼쪽 버튼"}
-							onClick={() => {
-								alert("왼쪽 클릭");
-							}}
-						/>
-					}
-					rightChild={
-						<Button
-							text={"오른쪽 버튼"}
-							onClick={() => {
-								alert("오른쪽 클릭");
-							}}
-						/>
-					}
-				/>
-
-				<h2>App.js</h2>
-
-				<Button
-					type={"positive"}
-					text={"Positive"}
-					onClick={() => {
-						alert("onClick 확인!");
-					}}
-				/>
-				<Button
-					type={"negative"}
-					text={"Negative"}
-					onClick={() => {
-						alert("onClick 확인!");
-					}}
-				/>
-				<Button
-					type={"default"}
-					text={"Default"}
-					onClick={() => {
-						alert("onClick 확인!");
-					}}
-				/>
-
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/detail/:id" element={<Detail />} />
-					<Route path="/new" element={<New />} />
-					<Route path="/edit" element={<Edit />} />
-				</Routes>
-			</div>
-		</BrowserRouter>
+		<DiaryStateContext.Provider value={list}>
+			<DiaryDispatchContext.Provider value={{ onCreate, onDelete, onEdit }}>
+				<BrowserRouter>
+					<div className="App">
+						<Routes>
+							<Route path="/" element={<Home />} />
+							<Route path="/detail/:id" element={<Detail />} />
+							<Route path="/new" element={<New />} />
+							<Route path="/edit" element={<Edit />} />
+						</Routes>
+					</div>
+				</BrowserRouter>
+			</DiaryDispatchContext.Provider>
+		</DiaryStateContext.Provider>
 	);
 }
 
