c096bdb039902e2f384f05b4d9d363cfe1276f9b
Max Chung <cwonho16@gmail.com>
2023-01-22 01:23:08 +0900

    feat: 공통으로 사용할 Button, Header 컴포넌트 제작

--- src/App.js
+++ src/App.js
@@ -5,10 +5,57 @@ import Detail from "./pages/Detail";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
 
+import Button from "./components/Button";
+import Header from "./components/Header";
+
 function App() {
 	return (
 		<BrowserRouter>
 			<div className="App">
+				<Header
+					headText={"App"}
+					leftChild={
+						<Button
+							text={"왼쪽 버튼"}
+							onClick={() => {
+								alert("왼쪽 클릭");
+							}}
+						/>
+					}
+					rightChild={
+						<Button
+							text={"오른쪽 버튼"}
+							onClick={() => {
+								alert("오른쪽 클릭");
+							}}
+						/>
+					}
+				/>
+
+				<h2>App.js</h2>
+
+				<Button
+					type={"positive"}
+					text={"Positive"}
+					onClick={() => {
+						alert("onClick 확인!");
+					}}
+				/>
+				<Button
+					type={"negative"}
+					text={"Negative"}
+					onClick={() => {
+						alert("onClick 확인!");
+					}}
+				/>
+				<Button
+					type={"default"}
+					text={"Default"}
+					onClick={() => {
+						alert("onClick 확인!");
+					}}
+				/>
+
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/detail/:id" element={<Detail />} />
---
+++ src/components/Button.js
@@ -0,0 +1,17 @@
+const Button = ({ type, text, onClick }) => {
+	const buttonType = ["positive", "negative"].includes(type) ? type : "default";
+	return (
+		<button
+			className={["button", `button_${buttonType}`].join(" ")}
+			onClick={onClick}
+		>
+			{text}
+		</button>
+	);
+};
+
+Button.defaultProps = {
+	type: "default",
+};
+
+export default Button;
