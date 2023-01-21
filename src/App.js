68c7b8b557caa416a3ba17471ebf7b9d5cda900c
Max Chung <cwonho16@gmail.com>
2023-01-21 18:24:42 +0900

    feat: 기본 페이지 라우팅 & 레이아웃 설정

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import New from "./pages/New";
+import Edit from "./pages/Edit";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<div className="App">
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/detail/:id" element={<Detail />} />
+					<Route path="/new" element={<New />} />
+					<Route path="/edit" element={<Edit />} />
+				</Routes>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
